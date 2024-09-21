0b0e9bf44492284144db6fcac7f7696086f1ca9e
migueldotnet <miguelarmarques.net@gmail.com>
2024-09-21 23:55:46 +0100

    update js custom action

--- .github/actions/js-dependency-update/index.js
+++ .github/actions/js-dependency-update/index.js
@@ -11,7 +11,7 @@ async function run()
         1.3 Github Token for authentication purposes (to create PRs)
         1.4 Working directory for which to check for updates
     2. Execute npm update command within the working the directory
-    3. Check wether there aremodified package*,json files
+    3. Check wether there are modified package*,json files
     4. If there are:
         4.1 Add and commit files to the target branch
         4.2 Create a PR to the base branch from the target branch
@@ -40,13 +40,34 @@ async function run()
 
     if (updateOutput.stdout.length > 0)
     {
-        core.info('There are updates available')
+        core.info('There are updates available');
+        await exec.exec('git checkout -b ' + target_branch);
+        await exec.exec('git add ' + target_branch + '/package*.json');
+        await exec.exec('git commit -m "updated project dependencies"');
+        await exec.exec('git push -u origin ' + target_branch);
+
+        const octokit = github.getOctokit(gh_token);
+
+        try
+        {
+            await octokit.rest.pulls.create({
+                owner: github.context.repo.owner,
+                repo: github.context.repo.repo,
+                title: 'Update NPM dependencies',
+                body: 'This pull request updates NPM packages',
+                base: base_branch,
+                head: target_branch
+            });
+        } catch (e)
+        {
+            core.info('Something went wrong when trying to create the PR');
+            core.setFailed(e.message);
+            core.error(e);
+        }
     } else
     {
         core.info('No updates at this point')
     }
-
-    core.info('I am a custom JS action')
 }
 
 run();
