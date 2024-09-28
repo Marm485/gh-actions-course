49b39956e2bf548d90b0d02ff335157c88d0f33f
migueldotnet <miguelarmarques.net@gmail.com>
2024-09-28 13:55:15 +0100

    add ping logic to docker action

--- .github/actions/docker-ping-url/main.py
+++ .github/actions/docker-ping-url/main.py
@@ -1,2 +1,29 @@
+import os
+import requests
+import time
+
+def ping_url(url, delay, max_trials):
+    number_of_trials = 0
+    while(number_of_trials < max_trials):
+        try:
+            response = requests.get(url)
+            if response.status_code == 200:
+                print(f"Website {url} is reachable")
+                return True
+        except requests.ConnectionError:
+            print(f"Website {url} is unreachable. Wating {delay} seconds before trying again.")
+            number_of_trials += 1
+            time.sleep(delay)
+            
+    return False
+
+def run():
+    website_url = os.environ['INPUT_URL']
+    delay = int(os.environ['INPUT_DELAY'])
+    max_trials = int(os.environ['INPUT_MAX_TRIALS'])
+    isWebsiteReachable = ping_url(website_url, delay, max_trials)
+    if not isWebsiteReachable:
+        raise Exception(f"Website {website_url} is unreachable or malformed.")
+
 if __name__ == '__main__':
-    print('Hello world!')
+    run()
