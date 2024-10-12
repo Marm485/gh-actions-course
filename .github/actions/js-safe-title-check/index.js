a1618ec1f22afd404d1aefc38f98a00701d5bde2
migueldotnet <miguelarmarques.net@gmail.com>
2024-10-12 13:05:08 +0100

    fix: 20-workflow-security: safe PR with JS custom action

--- .github/actions/js-safe-title-check/index.js
+++ .github/actions/js-safe-title-check/index.js
@@ -5,15 +5,15 @@ const exec = require('@actions/exec')
 async function run()
 {
 
-    var title = core.getInput('pr-title');
+    var title = core.getInput("pr-title");
 
-    if (title.startsWith('feat'))
+    if (title.startsWith("feat"))
     {
-        exec.exec('echo PR is a feature')
+        core.info("echo PR is a feature")
     }
     else
     {
-        exec.exec('PR is not a feature')
+        core.info("PR is not a feature")
     }
 };
 
